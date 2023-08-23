1b4a1128e0c7acaecf6744924458d529dcf55f1f
Luís Campanholi <lpcampanholi@gmail.com>
2023-08-23 08:45:46 -0300

    adicionando os arquivos do curso

---
+++ exercicio/main.js
@@ -0,0 +1,20 @@
+const listaDeTeclas = document.querySelectorAll("input[type=button]");
+const inputTel = document.querySelector("input[type=tel]");
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+  const tecla = listaDeTeclas[indice];
+
+  tecla.onclick = function () {
+    inputTel.value += tecla.value;
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
